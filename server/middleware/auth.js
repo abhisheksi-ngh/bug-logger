7a071e36abcbfa3570c19a8dca372fb08d2dc8a8
abhishekashj <abhishekkumrsingh9585@gmail.com>
2025-06-08 09:00:47 +0530

    clean-code

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,19 +1,3 @@
-// const jwt = require('jsonwebtoken');
-
-//    const auth = (req, res, next) => {
-//      const token = req.header('x-auth-token');
-//      if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
-
-//      try {
-//        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//        req.user = decoded;
-//        next();
-//      } catch (error) {
-//        res.status(401).json({ msg: 'Token is not valid' });
-//      }
-//    };
-
-//    module.exports = auth;
 const jwt = require('jsonwebtoken');
 const { logger } = require('../utils/logger');
 const { HttpError } = require('../utils/httpError');
